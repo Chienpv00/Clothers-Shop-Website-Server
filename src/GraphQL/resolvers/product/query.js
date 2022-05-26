95f6d6a5915f93426e504b3f2fd4912f19d265b9
ChienPV <chienpv00@gmail.com>
2022-05-26 15:08:01 +0700

    resolver query: getProductsForHome

--- src/GraphQL/resolvers/product/query.js
+++ src/GraphQL/resolvers/product/query.js
@@ -10,7 +10,15 @@ const productQueries = {
     getProductCat: async (_, {category, limit}, {dataSources})=>{
         const productArr = await dataSources.product.getProductCat(category, limit)
         return productArr
-    } 
+    },
+
+    getProductsForHome: async(_, __, {dataSources})=>{
+        const dataNew = await dataSources.product.getProductType('new', 5)
+        const dataHot = await dataSources.product.getProductType('hot', 5)
+        const dataNewImport = await dataSources.product.getProductType('newImport', 5)
+
+        return [dataNew, dataHot, dataNewImport]
+    }
 };
 
 module.exports = productQueries;
