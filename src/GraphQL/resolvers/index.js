880f00815b78d6c40ad434a9c8bfc2f19b44645a
ChienPV <chienpv00@gmail.com>
2022-05-26 15:05:56 +0700

    add productfields for resolver chain

--- src/GraphQL/resolvers/index.js
+++ src/GraphQL/resolvers/index.js
@@ -1,10 +1,12 @@
 const productQueries = require('./product/query')
+const productFields = require('./product/field')
 
 const resolvers = {
     Query: {
         ...productQueries
-    }
+    },
 
+    ...productFields
 }
 
 module.exports = resolvers;
