8ec69a5ff0a9f99779f144d9ad4b901d01886483
ChienPV <chienpv00@gmail.com>
2022-06-08 16:27:12 +0700

    chien - transfer: mongodb local -> mongodb atlas

--- src/db/index.js
+++ src/db/index.js
@@ -1,8 +1,31 @@
 const mongoose = require('mongoose');
 
 const connect = async () => {
+
+    // environment
+    const userName = 'chien';
+    const password = encodeURI('chien1312');
+    const dbName = 'clothes';
+
+    const uriConnect =
+        'mongodb+srv://' +
+        userName +
+        ':' +
+        password +
+        '@cluster0.g4q7s.mongodb.net/' +
+        dbName +
+        '?retryWrites=true&w=majority';
+    // optional params
+    const params = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    };
+
     try {
-        const db = await mongoose.connect('mongodb://localhost:27017/clothes');
+        const db = await mongoose.connect(
+            uriConnect,
+            params
+        );
         console.log('🚀🚀 Connection successfully!!!');
         return db;
     } catch (error) {
